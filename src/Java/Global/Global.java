236c741ed73711427b54d5ee344bdb1b3d44e44c
APassbyDreg <36123017+APassbyDreg@users.noreply.github.com>
2019-11-01 20:41:42 +0800

    bug fix / add feature / new release

---
+++ src/Java/Global/Global.java
@@ -0,0 +1,17 @@
+package Global;
+
+public class GlobalVariables {
+    public static String programAbsolutePath;
+
+    public static final int MAX_RECENT_FILES_STORED = 4;
+    public static final String programName = "MDEditTool";
+    public static final String jarName = "MarkdownEditTool";
+    public static final String themesFolderRelativePath = "\\resources\\themes\\";
+    public static final String logoRelativePath = "Logo.png";
+    public static final String introPageDesignPath = "IntroPageDesign.css";
+    public static final String editPageDesignPath = "EditPageDesign.css";
+    public static final String introFXMLPath = "/fxml/GUI_intro.fxml";
+    public static final String editFXMLPath = "/fxml/GUI_edit.fxml";
+    public static final String fontPath = "sarasa-monoT-sc-semibold.ttf";
+
+}
